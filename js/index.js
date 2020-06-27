deb1cf567f26c24105ae8a5205c5672744466ea2
Hasan <hasan_skaterboy@hotmail.com>
2020-06-26 23:49:59 -0400

    even more

--- js/index.js
+++ js/index.js
@@ -1,39 +1,5 @@
-function loop_through_cards() {
-    let cards = document.getElementById("card-holder").childNodes;
-    console.log(cards.length);
 
-    for (let i = 0; i < cards.length; ++i) {
-        // console.log("on " + cards[i]);
-        // console.log(cards[i].nodeName);
-
-        if (cards[i].nodeName.toLowerCase() === "div") {
-            // console.log("we found a div. it has children: ");
-            // console.log(cards[i].childNodes);
-            let card_children = cards[i].childNodes;
-            for (let j = 0; j < card_children.length; ++j) {
-                // console.log("the current child is " + card_children[j].nodeName);
-                child_children = card_children[j].childNodes;
-                for (let z = 0; z < child_children.length; z++) {
-                    // console.log(child_children[z].nodeName);
-                    if (child_children[z].nodeName.toLowerCase() === "a") {
-                        console.log("we found an a");
-                        let a_children = child_children[z].childNodes;
-                        let image = child_children[z].childNodes[0];
-                        image.src = "pics/alexa/one.jpg";
-                        console.log("we found a " + image.nodeName);
-
-                    }
-                }
-
-            }
-        }
-
-
-    }
-    
-}
-
-function loop_through_photos() {
+function get_photo_urls() {
     let image_holder = [];
     let photo_api_url = "http://" + window.location.hostname + ":5005/all_pics"
     console.log("sending request to " + photo_api_url);
@@ -44,8 +10,43 @@ function loop_through_photos() {
     .done(function(response_data, resonse_status, response_xhr) {
         console.log("we out here!");
         console.log(response_data);
+        return response_data["pic_array"];
     })
     .fail(function(response_data, response_status, response_xhr) {
         console.log("we failed!");
+        return null;
     })
+}
+
+function loop_through_cards() {
+    let cards = document.getElementById("card-holder").childNodes;
+    console.log(cards.length);
+    let all_photos = get_photo_urls();
+    let amount_of_photos = all_photos.length();
+    if (all_photos != null) {
+        for (let i = 0; i < cards.length; ++i) {
+            // console.log("on " + cards[i]);
+            // console.log(cards[i].nodeName);
+            if (cards[i].nodeName.toLowerCase() === "div") {
+                // console.log("we found a div. it has children: ");
+                // console.log(cards[i].childNodes);
+                let card_children = cards[i].childNodes;
+                for (let j = 0; j < card_children.length; ++j) {
+                    // console.log("the current child is " + card_children[j].nodeName);
+                    child_children = card_children[j].childNodes;
+                    for (let z = 0; z < child_children.length; z++) {
+                        // console.log(child_children[z].nodeName);
+                        if (child_children[z].nodeName.toLowerCase() === "a") {
+                            console.log("we found an a");
+                            let a_children = child_children[z].childNodes;
+                            let image = child_children[z].childNodes[0];
+
+                            image.src = "pics/alexa/one.jpg";
+                            console.log("we found a " + image.nodeName);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
