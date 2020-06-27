c3b6662e3bca1d454b858febed9860ef8d5df778
Hasan <hasan_skaterboy@hotmail.com>
2020-06-26 23:46:25 -0400

    even more

--- fs_api.py
+++ fs_api.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route("/")
 def root():
