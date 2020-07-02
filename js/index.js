c04fbe4946d20e134b1b5d72a218392bf6a714c9
Hasan <hasan_skaterboy@hotmail.com>
2020-07-01 22:06:27 -0400

    more

--- js/index.js
+++ js/index.js
@@ -18,29 +18,30 @@ function get_photo_urls() {
             // console.log("there are " + amount_of_pics + " pics");
             if (pics_array != null) {
 
+                let outside_holder = document.getElementById("card-holder"); 
                 let card_div = document.createElement("div");
-                    card_div.classList.add("col-lg-4");
-                    card_div.classList.add("col-md-6");
-                    card_div.classList.add("mb-4");
-                    let inner_div = document.createElement("div");
-                    inner_div.classList.add("card");
-                    inner_div.classList.add("h-100");
-                    let pic_a = document.createElement("a");
-                    let pic = document.createElement("img");
-                    pic.classList.add("card-img-top");
-                    let card_body = document.createElement("div");
-                    card_body.classList.add("card-body");
-                    let name_header_four = document.createElement("h4");
-                    name_header_four.classList.add("card-title");
-                    let name_a = document.createElement("a");
-                    name_a.innerHTML = "HI I AM ALIVE";
-                    name_header_four.appendChild(name_a);
-                    card_body.appendChild(name_header_four);
-                    pic_a.appendChild(pic);
-                    inner_div.appendChild(pic_a);
-                    inner_div.appendChild(card_body);
-                    // /*
-                    outside_holder.appendChild(card_div);
+                card_div.classList.add("col-lg-4");
+                card_div.classList.add("col-md-6");
+                card_div.classList.add("mb-4");
+                let inner_div = document.createElement("div");
+                inner_div.classList.add("card");
+                inner_div.classList.add("h-100");
+                let pic_a = document.createElement("a");
+                let pic = document.createElement("img");
+                pic.classList.add("card-img-top");
+                let card_body = document.createElement("div");
+                card_body.classList.add("card-body");
+                let name_header_four = document.createElement("h4");
+                name_header_four.classList.add("card-title");
+                let name_a = document.createElement("a");
+                name_a.innerHTML = "HI I AM ALIVE";
+                name_header_four.appendChild(name_a);
+                card_body.appendChild(name_header_four);
+                pic_a.appendChild(pic);
+                inner_div.appendChild(pic_a);
+                inner_div.appendChild(card_body);
+                // /*
+                outside_holder.appendChild(card_div);
                     // */
 
                 /*
